5bbc652231511d64c1ac134cdab0aa26e1641a06
Ludovico Russo <ludus.russo@gmail.com>
2022-06-27 20:07:02 +0200

    handle vercel

--- src/api/telegram/[...telegraf].ts
+++ src/api/telegram/[...telegraf].ts
@@ -1,5 +1,3 @@
-import fastify from "fastify";
-
 import { createBot } from "../../bot";
 
 const botToken = process.env.TELEGRAM_BOT_TOKEN;
@@ -14,11 +12,7 @@ if (!whBaseUrl) {
   process.exit(1);
 }
 
-const port = Number(process.env.PORT) || 3000;
-
 const bot = createBot(botToken);
-
-const app = fastify();
 
 const path = `/telegram/${bot.secretPathComponent()}`;
 const url = new URL(path, whBaseUrl).href;
@@ -27,13 +21,6 @@ bot.telegram.setWebhook(url).then(() => {
   console.log("Webhook is set!: ", url);
 });
 
-app.post(path, (req, rep) => bot.handleUpdate(req.body as any, rep.raw));
-
-app
-  .listen({
-    host: "0.0.0.0",
-    port: port,
-  })
-  .then(() => {
-    console.log("🚀 Listening on port: " + port);
-  });
+export default function handler(req: any, rep: any) {
+  bot.handleUpdate(req.body as any, rep.raw);
+}

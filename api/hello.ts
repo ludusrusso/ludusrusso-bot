c57469db8110a37d442f4249a9868155b2355c85
Ludovico Russo <ludus.russo@gmail.com>
2022-06-28 19:33:28 +0200

    add hello test

---
+++ api/hello.ts
@@ -0,0 +1,6 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+
+// We only keep this in for testing purposes.
+export default (req: VercelRequest, res: VercelResponse) => {
+  return res.send("Hello World");
+};
